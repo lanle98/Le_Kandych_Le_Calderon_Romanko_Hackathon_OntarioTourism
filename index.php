8cd5376da30ab048e6959197b4875d6f59a0f386
Darren Le <darrenle98@gmail.com>
2020-02-10 20:22:33 -0500

    added file

--- index.php
+++ index.php
@@ -29,16 +29,16 @@ require_once 'load.php';
             <div class="header">
                 <!-- Navbar brand -->
                 <nav>
-                    <a class="navbar-brand" href="#"><img src="images/Logo.svg" alt="logo" /></a>
+                    <a class="navbar-brand" href="/"><img src="images/Logo.svg" alt="logo" /></a>
 
                     <!-- Collapse button -->
-                    <button class="navbar-toggler first-button mr-auto align-center" type="button" data-toggle="collapse" data-target="#navbarSupportedContent20" aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
+                    <div class="navbar-toggler first-button mr-auto align-center" type="button" data-toggle="collapse" data-target="#navbarSupportedContent20" aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
                         <div class="animated-icon">
                             <span></span>
                             <span></span>
                             <span></span>
                         </div>
-                    </button>
+                    </div>
 
                     <!-- Collapsible content -->
                     <div class="collapse navbar-collapse " id="navbarSupportedContent20">
@@ -76,41 +76,9 @@ require_once 'load.php';
         </div>
 
 
-
+        <router-view></router-view>
     </div>
 
-    <p class="welcome-text text-left m-lg-5">
-        Everything in Canada inevitably leads back to Ontario. The country’s most populated province by a wide margin, one in three Canadians dwell within its borders, along with practically everything powerful and important. Among other landmarks, Ontario houses Canada’s parliament, its largest city, the national stock exchange, the country’s biggest university, the headquarters of virtually every major Canadian newspaper, television station, corporation, and bank, and — just to top it off — Canada’s tallest building and biggest waterfall, too.
-    </p>
-    <div class="travel-ideas display">
-        <h2 class="text-center"> TRAVEL IDEAS</h2>
-        <div>
-            <h2>
-                Looking for the perfect advanture?
-            </h2>
-            <div class="yellow-div position-relavite"></div>
-            <img src="/images/lakes.jpg" alt="cities" class="position-absolute image-fluid" />
-
-        </div>
-
-        <div>
-
-            <div class="yellow-div position-relavite"></div>
-            <img src="/images/lakes.jpg" alt="cities" class="position-absolute image-fluid" />
-            <p>
-                Ontario’s power comes at a price, however, and Canadians who live outside its borders will often regard it with a mixture of resentment and skepticism — surely they don’t deserve all that.
-            </p>
-
-        </div>
-
-    </div>
-    <router-view></router-view>
-
-
-
-
-
-    </div>
 
 
 
