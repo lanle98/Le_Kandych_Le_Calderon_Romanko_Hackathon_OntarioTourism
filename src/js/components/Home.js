79d20d95c4e99083cbd4323816cfcb2d507cce76
Darren Le <darrenle98@gmail.com>
2020-02-10 22:41:46 -0500

    css for home

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,40 +1,5 @@
 export default {
   template: `
-    <div class="home">
-    <p class="welcome-text text-left m-lg-5">
-        Everything in Canada inevitably leads back to Ontario. The country’s most populated province by a wide margin, one in three Canadians dwell within its borders, along with practically everything powerful and important. Among other landmarks, Ontario houses Canada’s parliament, its largest city, the national stock exchange, the country’s biggest university, the headquarters of virtually every major Canadian newspaper, television station, corporation, and bank, and — just to top it off — Canada’s tallest building and biggest waterfall, too.
-    </p>
-    <div class="travel-ideas display">
-        <h2 class="text-center"> TRAVEL IDEAS</h2>
-        <div>
-            <h2>
-                Looking for the perfect advanture?
-            </h2>
-            <div class="yellow-div position-relavite"></div>
-            <img src="/images/lakes.jpg" alt="cities" class="position-absolute image-fluid" />
-
-        </div>
-
-        <div>
-
-            <div class="yellow-div position-relavite"></div>
-            <img src="/images/lakes.jpg" alt="cities" class="position-absolute image-fluid" />
-            <p>
-                Ontario’s power comes at a price, however, and Canadians who live outside its borders will often regard it with a mixture of resentment and skepticism — surely they don’t deserve all that.
-            </p>
-
-        </div>
-
-    </div>
-
-
-
-
-
-
-    </div>
-    </div>
-
-    
+ 
     `
 };
