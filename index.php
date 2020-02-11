3b5daaf73c089ac712890c78266dd898b1e9cccc
Vira <vira.romanko99@gmail.com>
2020-02-10 20:12:41 -0500

    style

--- index.php
+++ index.php
@@ -108,24 +108,28 @@ require_once 'load.php';
         <p class="welcome-text text-left m-lg-5">
         Everything in Canada inevitably leads back to Ontario. The country’s most populated province by a wide margin, one in three Canadians dwell within its borders, along with practically everything powerful and important. Among other landmarks, Ontario houses Canada’s parliament, its largest city, the national stock exchange, the country’s biggest university, the headquarters of virtually every major Canadian newspaper, television station, corporation, and bank, and — just to top it off — Canada’s tallest building and biggest waterfall, too.
         </p>
-        <div class="travel-ideas display">
+        <div class="travel-ideas d-flex flex-column">
             <h2 class="text-center"> TRAVEL IDEAS</h2>
             <div>
                 <h2>
                     Looking for the perfect advanture?
                 </h2>
-                   <div class="yellow-div position-relavite"></div>
-                   <img src="/images/lakes.jpg" alt="cities" class="position-absolute image-fluid"/>
+                   <div class="yellow-div "></div>
+                   <img src="/images/cities.jpg" alt="cities" class=" image-fluid"/>
 
             </div>
 
             <div>
                 
-                   <div class="yellow-div position-relavite"></div>
-                   <img src="/images/lakes.jpg" alt="cities" class="position-absolute image-fluid"/>
+                   <div class="yellow-div "> </div>
+                   <img src="/images/lakes.jpg" alt="cities" class=" image-fluid"/>
                    <p>
                    Ontario’s power comes at a price, however, and Canadians who live outside its borders will often regard it with a mixture of resentment and skepticism — surely they don’t deserve all that.
                </p>
+
+            </div>
+            <div class="where-to-go">
+            <h2 class="text-center"> WHERE TO GO</h2>
 
             </div>
 
